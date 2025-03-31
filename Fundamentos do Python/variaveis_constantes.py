80082c221b1f12bcb6286776f52f19fc6761b3be
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-03-30 21:42:00 -0300

    Variáveis e Constantes

---
+++ Fundamentos do Python/variaveis_constantes.py
@@ -0,0 +1,14 @@
+#Variáveis
+nome = "Iran"
+idade = 32
+
+nome, idade = ("Guilherme", 27)
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+
+
+#Constantes sempre com letras maiúsculas
+BRASILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+print(BRASILIAN_STATES)
